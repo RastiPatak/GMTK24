104f1971e0175357578cc36d6df7ca9aad5ee3d6
Michael Steinmötzger <m.steinmoetzger@gmail.com>
2024-08-16 21:29:52 +0200

    Add Resizable

---
+++ Assets/Scripts/Scaling/Resizable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scaling
+{
+    public class Resizable : MonoBehaviour
+    {
+
+        private float _currentScale = 1;
+
+        public double HealthMultiplier => 2 * Math.Log(_currentScale) + 1f;
+        
+        public void Resize(float scale)
+        {
+            this._currentScale = scale;
+            gameObject.transform.localScale = new Vector3(scale, scale, scale);
+        }
+
+        
+    }
+    
+    
+}
